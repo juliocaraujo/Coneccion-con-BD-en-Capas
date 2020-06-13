7201789dbd878cef54614e74b75463755e07d452
Julio Araujo <julio.araujo@adinet.com.uy>
2020-06-13 15:11:49 -0300

    Primer comit

---
+++ CapaDeDatos/Modelos/ModeloPersona.vb
@@ -0,0 +1,20 @@
+﻿Public Class ModeloPersona
+    Inherits Modelo
+    'hereda todo de Modelo
+    Public Id As String
+    Public Nombre As String
+    Public Apellido As String
+    Public Mail As String
+
+    Public Function Contar()
+        Me.Comando.CommandText = "SELECT COUNT(*) FROM persona"
+        Return Me.Comando.ExecuteScalar().ToString()
+    End Function
+
+    Public Sub Guardar()
+        Comando.CommandText = "INSERT INTO persona(id,nombre,apellido,mail) 
+                            VALUES (" + Me.Id + ",'" + Me.Nombre + "','" + Me.Apellido + "','" + Me.Mail + "')"
+        Comando.ExecuteNonQuery()
+    End Sub
+
+End Class
---
+++ CapaLogica/ControladorPersona.vb
@@ -0,0 +1,11 @@
+﻿Imports CapaDeDatos
+
+Public Module ControladorPersona
+
+    Public Function ObtenerCantidad()
+        Dim p As New ModeloPersona
+        Return p.Contar()
+
+    End Function
+
+End Module
